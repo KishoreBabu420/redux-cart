5fe90d2f6c27369c29a7358847ddd9097c2d71c3
kishore <kishore.sai37@gmail.com>
2022-08-04 16:44:13 +0530

    add cartItem

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
-function App() {
-  return <h2>Redux Toolkit</h2>;
-}
+import React from 'react';
+import Navbar from './components/Navbar';
+import CartContainer from './components/CartContainer';
+
+const App = () => {
+  return (
+    <main>
+      <Navbar />
+      <CartContainer />
+    </main>
+  );
+};
+
 export default App;
